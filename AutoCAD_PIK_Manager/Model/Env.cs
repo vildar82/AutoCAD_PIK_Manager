ee1f851d6f8f45aa153cfa305a3d6aef7c4d7990
Vildar Khisyametdinov <vildar82@gmail.com>
2016-05-25 18:25:55 +0300

    CadManLogin

--- AutoCAD_PIK_Manager/Model/Env.cs
+++ AutoCAD_PIK_Manager/Model/Env.cs
@@ -6,34 +6,36 @@ using System.Text;
 using AutoCadApp = Autodesk.AutoCAD.ApplicationServices.Application;
 
 namespace AutoCAD_PIK_Manager
-{ 
-   public static class Env
-   {
-      public static int Ver = AutoCadApp.Version.Major;
-      // AutoCAD 2007...2012
-      [DllImport("acad.exe", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "acedGetEnv")]
-      extern static private Int32 acedGetEnv12(string var, StringBuilder val);
-      // AutoCAD 2013...
-      [DllImport("accore.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "acedGetEnv")]
-      extern static private Int32 acedGetEnv13(string var, StringBuilder val);
+{
+    public static class Env
+    {
+        public static int Ver = AutoCadApp.Version.Major;
+        public static string CadManLogin { get; } = Settings.PikSettings.PikFileSettings?.LoginCADManager;
 
-      static public string GetEnv(string var)
-      {
-         StringBuilder val = new StringBuilder(16536);
-         if (Ver <= 18) acedGetEnv12(var, val); else acedGetEnv13(var, val);
-         return val.ToString();
-      }
-      
-      // AutoCAD 2007...2012
-      [DllImport("acad.exe", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "acedSetEnv")]
-      extern static private Int32 acedSetEnv12(string var, string val);
-      // AutoCAD 2013...
-      [DllImport("accore.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "acedSetEnv")]
-      extern static private Int32 acedSetEnv13(string var, string val);
+        // AutoCAD 2007...2012
+        [DllImport("acad.exe", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "acedGetEnv")]
+        extern static private Int32 acedGetEnv12(string var, StringBuilder val);
+        // AutoCAD 2013...
+        [DllImport("accore.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "acedGetEnv")]
+        extern static private Int32 acedGetEnv13(string var, StringBuilder val);
 
-      static public void SetEnv(string var, string val)
-      {
-         if (Ver <= 18) acedSetEnv12(var, val); else acedSetEnv13(var, val);
-      }
-   }
+        static public string GetEnv(string var)
+        {
+            StringBuilder val = new StringBuilder(16536);
+            if (Ver <= 18) acedGetEnv12(var, val); else acedGetEnv13(var, val);
+            return val.ToString();
+        }
+
+        // AutoCAD 2007...2012
+        [DllImport("acad.exe", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "acedSetEnv")]
+        extern static private Int32 acedSetEnv12(string var, string val);
+        // AutoCAD 2013...
+        [DllImport("accore.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "acedSetEnv")]
+        extern static private Int32 acedSetEnv13(string var, string val);
+
+        static public void SetEnv(string var, string val)
+        {
+            if (Ver <= 18) acedSetEnv12(var, val); else acedSetEnv13(var, val);
+        }
+    }
 }
