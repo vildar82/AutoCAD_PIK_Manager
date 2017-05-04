8638fbb538182eeb934ba35b440cd6749f7e26a9
Vildar <vildar82@gmail.com>
2017-05-04 10:43:34 +0300

    epplus v4.1

--- AutoCAD_PIK_Manager/Properties/AssemblyInfo.cs
+++ AutoCAD_PIK_Manager/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.111.124")]
-[assembly: AssemblyFileVersion("1.0.111.124")]
+[assembly: AssemblyVersion("1.0.124.125")]
+[assembly: AssemblyFileVersion("1.0.124.125")]
